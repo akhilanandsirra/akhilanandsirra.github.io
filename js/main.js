eeaf517afb45050d7e3695ffeaa04b8e90d1a07e
Akhil <akhilanand9999@gmail.com>
2019-10-16 12:57:35 +0530

    navbar updated

--- js/main.js
+++ js/main.js
@@ -2,7 +2,3 @@ $('.navbar-nav .nav-link').click(function () {
     $('.navbar-nav .nav-link').removeClass('active');
     $(this).addClass('active');
 })
-
-$('.navbar .navbar-brand').click(function () {
-    $('.navbar-nav .nav-link').removeClass('active');
-})
