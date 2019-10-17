0d6c32e3339de3607dad403d09e96366c5569d73
Akhil <akhilanand9999@gmail.com>
2019-10-17 16:03:20 +0530

    preloader added

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ $('.navbar-nav .nav-link').click(function () {
     $('.navbar-nav .nav-link').removeClass('active');
     $(this).addClass('active');
 })
+//preloader
+setTimeout(() => {
+    $("#status").fadeOut();
+    $("#preloader").delay(500).fadeOut("slow");
+}, 750);
+
