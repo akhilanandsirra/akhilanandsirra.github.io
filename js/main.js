74fedf5a0ee33e79b6af49c6156b16924b1dce77
Akhil <akhilanand9999@gmail.com>
2019-10-19 23:28:07 +0530

    description updated

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ $('a.smoth-scroll').on('click', function (e) {
     var anchor = $(this);
     $('html, body').stop().animate({
         scrollTop: $(anchor.attr('href')).offset().top - 50
-    }, 1000);
+    }, 250);
     e.preventDefault();
 });
 
