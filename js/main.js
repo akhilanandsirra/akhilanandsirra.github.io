e62128ef321bd6e2d57b0977cd5c5255ae4420ac
Akhil <akhilanand9999@gmail.com>
2019-10-20 15:43:41 +0530

    resume button added

--- js/main.js
+++ js/main.js
@@ -77,3 +77,12 @@ $('.nav-link').on('click', function (e) {
         scrollTop: $($(elem).attr('href')).offset().top
     }, 250);
 });
+
+$('.about').on('click', function (e) {
+    e.preventDefault();
+    const elem = this; // save it so we can use it in the animate
+
+    $('html, body').animate({
+        scrollTop: $($(elem).attr('href')).offset().top
+    }, 250);
+});
